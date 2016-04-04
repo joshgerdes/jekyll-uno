d54d82addd76c21e3688315110e7721a678324aa
Vince Tse <thelazyenginerd@gmail.com>
2016-04-04 15:45:39 -0400

    Got blog animation working

--- js/main.js
+++ js/main.js
@@ -2,31 +2,36 @@
 layout: null
 ---
 $(document).ready(function () {
+  // blog button click
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
-      $('a.blog-button').attr('href', '/')
-      return
-    }
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-      $('a.blog-button').attr('href', '/')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeOut')
+      $('.content-wrapper').addClass('animated slideOutRight')
+      width = $(window).width()
+      $('.panel-cover').animate({'max-width': width + 'px', 'width': '100%'}, 400, swing = 'swing', function () {
+        window.location.replace('/')
+      })
+      $('.panel-cover').removeClass('panel-cover--collapsed')
     } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      currentWidth = $('.panel-cover').width()
+      if (currentWidth < 960) {
+        $('.content-wrapper').addClass('animated slideInRight')
+        $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeOut')
+      } else {
+        $('.panel-cover').css('max-width', currentWidth)
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      }
+      $('.panel-cover').addClass('panel-cover--collapsed')
     }
   })
 
+  // direct hashing to the blogs screen
   if (window.location.hash && window.location.hash == '#blogs') {
     $('.panel-cover').addClass('panel-cover--collapsed')
-    $('a.blog-button').attr('href', '/')
   }
 
+  // default to home path
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-    $('a.blog-button').attr('href', '/')
+    window.location.replace('/')
   }
 
   $('.btn-mobile-menu').click(function () {

e29bf5653ef26503d16a78714a5a6e62794caf6e
Luke Murray <lmurr011cladmin@msd.govt.nz>
2023-04-29 09:41:17 +1200

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master"
 
 export default defineConfig({
   branch,
-  clientId: "0861defe-27c3-43f8-aa1a-d4ce9370529b", // Get this from tina.io
-  token: "ea14c92e7cd34ff2728b4ca00b1fabb8b19b72cd", // Get this from tina.io
+  clientId: "5abfd70b-e8cf-44a8-8376-f24439935163", // Get this from tina.io
+  token: "7880906cadaf177cb2431fb058964d9113094537", // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
